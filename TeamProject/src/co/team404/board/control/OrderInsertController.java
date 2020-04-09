b48c812eab9952f16042f0b8c5044706c0c127f4
User <User@YD01-18>
2020-04-09 16:08:08 +0900

    OrderInsertController.java

--- TeamProject/src/co/team404/board/control/OrderInsertController.java
+++ TeamProject/src/co/team404/board/control/OrderInsertController.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class OrderPage
  */
 @WebServlet("/orderPage.do")
-public class OrderPage extends HttpServlet {
+public class OrderInsertController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public OrderPage() {
+    public OrderInsertController() {
         super();
         // TODO Auto-generated constructor stub
     }
