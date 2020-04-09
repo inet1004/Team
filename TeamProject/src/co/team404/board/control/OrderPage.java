dde610bb95599e497a2b0ef3de083ef39c571c69
User <User@YD01-18>
2020-04-09 16:01:20 +0900

    OrderPage.java

--- TeamProject/src/co/team404/board/control/OrderPage.java
+++ TeamProject/src/co/team404/board/control/OrderPage.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class OrderPage
  */
-@WebServlet("/OrderPage.do")
+@WebServlet("/orderPage.do")
 public class OrderPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
